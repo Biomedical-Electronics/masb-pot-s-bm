011865bf6941563df2c7133b7ed8d38856c59205
mariamor <mmorbasa7@alumnes.ub.edu>
2021-05-15 11:20:39 +0200

    Archivos stm32main

---
+++ masb-pot-s-firmware/Core/Inc/components/stm32main.h
@@ -0,0 +1,13 @@
+/*
+ * stm32main.h
+ *
+ *  Created on: 15 may. 2021
+ *      Author: mariamor
+ */
+
+#ifndef INC_COMPONENTS_STM32MAIN_H_
+#define INC_COMPONENTS_STM32MAIN_H_
+
+
+
+#endif /* INC_COMPONENTS_STM32MAIN_H_ */
---
+++ masb-pot-s-firmware/Core/Src/components/stm32main.c
@@ -0,0 +1,8 @@
+/*
+ * stm32main.c
+ *
+ *  Created on: 15 may. 2021
+ *      Author: mariamor
+ */
+
+
