c0f48dc02ad32e187e170741315adaad1f484762
mariamor <mmorbasa7@alumnes.ub.edu>
2021-05-15 17:54:45 +0200

    PMU, DAC y timers configurados

--- masb-pot-s-firmware/Core/Inc/components/stm32main.h
+++ masb-pot-s-firmware/Core/Inc/components/stm32main.h
@@ -8,6 +8,17 @@
 #ifndef INC_COMPONENTS_STM32MAIN_H_
 #define INC_COMPONENTS_STM32MAIN_H_
 
+#include "stm32f4xx_hal.h"
 
+struct Handles_S {
+	UART_HandleTypeDef *huart;
+	ADC_HandleTypeDef *hadc1;
+	I2C_HandleTypeDef *hi2c1;
+	TIM_HandleTypeDef *htim3;
+};
+
+// Prototypes.
+void setup(struct Handles_S *handles);
+void loop(void);
 
 #endif /* INC_COMPONENTS_STM32MAIN_H_ */
